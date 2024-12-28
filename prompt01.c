5f7422935ccb2520581e5b98c30bc88b563e7739
nouhailaaleen <173621209+niylii@users.noreply.github.com>
2024-12-28 01:30:00 +0100

    [update - fixed a lllot but not the path crap (>H<)]

--- prompt01.c
+++ prompt01.c
@@ -1,32 +1,36 @@
 #include "main.h"
+/**
+ * @environ - 
+ */
 
+extern char **environ;
 /**
  * prompt - function that shows the prompt and
  *	takes the command then execute it
  */
-void prompt(void)
+
+void simple_shell(void)
 {
-	char *cmd_in = NULL;
-	int cmd_stat, exec_stat;
+	char *cmd = NULL;
+	int exec_stat;
 	size_t n = 0;
+	ssize_t cmd_stat;
 	pid_t pid;
 
-	write(1, "$ ", 2);
-	cmd_stat = getline(&cmd_in, &n, stdin);
-	while (cmd_stat == -1)
+	while (1)
 	{
-		free(cmd_in);
-		perror("no command is inserted");
-		cmd_stat = getline(&cmd_in, &n, stdin);
-	}
-	if (cmd_stat != -1)
-	{
+		write(1, "$ ", 2);
+		cmd_stat = getline(&cmd, &n, stdin);
+		if (cmd_stat == -1)
+		{
+			e_of(cmd);
+			perror("no command is inserted");
+		}
 		pid = fork();
 		if (pid == 0)
 		{
-			char *args[] = {cmd_in, NULL};
-
-			if (execve(cmd_in, args, NULL) == -1)
+			char *args[] = {cmd, NULL};
+			if (execve(cmd, args, environ) == -1)
 				perror("execution faild!");
 			exit(0);
 		}
@@ -34,5 +38,5 @@ void prompt(void)
 			perror("process not created");
 		if (pid > 0)
 			wait(&exec_stat);
-	}
+		}
 }
