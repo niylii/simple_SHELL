3b4751aafc8cb6d16392dc5cd493c726993283f4
nouhailaaleen <173621209+niylii@users.noreply.github.com>
2024-12-28 16:59:42 +0100

    [update - betty doc + \n rmvd]

--- R_functions.c
+++ R_functions.c
@@ -1,11 +1,16 @@
 #include "main.h"
 
+/**
+ * e_of - Check for end-of-file (EOF) and handle cleanup.
+ * @file: A pointer to a dynamically allocated string (file name or path).
+ */
 void e_of(char *file)
 {
 	if (feof(stdin))
 	{
 		free(file);
-		write(STDOUT_FILENO, "\n", 1);
-		exit(0);
+		write(STDOUT_FILENO, "", 1);
+		exit(EXIT_SUCCESS);
 	}
 }
+
