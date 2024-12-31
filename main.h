09a3f2a92b1a42bd98b205389dcf4f634e10827e
nouhailaaleen <173621209+niylii@users.noreply.github.com>
2024-12-31 23:19:17 +0100

    [update - functions are added]

--- main.h
+++ main.h
@@ -9,9 +9,24 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 
+/**environ variable**/
+extern char **environ;
+
+/**for simple shell versions**/
 void simple_shell(void);
 void simple_shell02(void);
+void simple_shell03(void);
+
+/**for certen cases**/
 void e_of(char *);
 char *tokenize_pro_name(char **, char **);
+
+/**for environement variables**/
+int env_exists(const char *name);
+char *get_env(const char *name);
+char *cmd_type(char *pro);
+/**for strings manupilation**/
+char *str_find(char *str, char dil);
 char *str_cspn(char *);
+int str_ncpm(char *s1, char *s2);
 #endif
