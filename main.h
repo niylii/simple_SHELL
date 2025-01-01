179232663555746749ca1aaaa5660d3aae18d69c
nouhailaaleen <173621209+niylii@users.noreply.github.com>
2025-01-01 22:11:23 +0100

    [update - proto is addaed]

--- main.h
+++ main.h
@@ -19,12 +19,16 @@ void simple_shell03(void);
 
 /**for certen cases**/
 void e_of(char *);
+void getline_check(ssize_t stat, char *cmd);
+void exec_process(char *cmd_path, char **args, int exec_stat, char *cmd, pid_t pid);
 char *tokenize_pro_name(char **, char **);
 
 /**for environement variables**/
 int env_exists(const char *name);
 char *get_env(const char *name);
 char *cmd_type(char *pro);
+
+/****/
 /**for strings manupilation**/
 char *str_find(char *str, char dil);
 char *str_cspn(char *);
