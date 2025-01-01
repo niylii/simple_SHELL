516cddbd6af12f415a9600dded964eb59dfc3029
nouhailaaleen <173621209+niylii@users.noreply.github.com>
2025-01-01 22:38:42 +0100

    [betty - under 50]

--- prompt03.c
+++ prompt03.c
@@ -18,37 +18,14 @@ void simple_shell03(void)
 	{
 		write(STDIN_FILENO, "$ ", 2);
 		cmd_stat = getline(&cmd, &n, stdin);
-		if (cmd_stat == -1)
-		{
-			e_of(cmd);
-			perror("getline failed");
-			break;
-		}
+		getline_check(cmd_stat, cmd);
 		cmd = str_cspn(cmd);
 		if (!cmd || cmd[0] == '\0')
 			continue;
 		pro_name = tokenize_pro_name(&cmd, args);
 		cmd_path = cmd_type(pro_name);
 		if (cmd_path)
-		{
-			pid = fork();
-			if (pid < 0)
-			{
-				perror("fork failed");
-				continue;
-			}
-			if (pid == 0)
-			{
-				if (execve(cmd_path, args, environ) == -1)
-				{
-					perror("execution failed");
-					free(cmd);
-					exit(EXIT_FAILURE);
-				}
-			}
-			if (pid > 0)
-				waitpid(pid, &exec_stat, 0);
-		}
+			exec_process(cmd_path, args, exec_stat, cmd, pid);
 		else
 			write(STDERR_FILENO, "Command not found\n", 18);
 
