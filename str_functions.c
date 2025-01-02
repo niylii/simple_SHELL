594e74000983aabfd9b4eedd7eaf8fef697e4e7b
nouhailaaleen <173621209+niylii@users.noreply.github.com>
2025-01-02 21:45:51 +0100

    str_cmp nd len fixed

--- str_functions.c
+++ str_functions.c
@@ -9,7 +9,7 @@ int str_len(char *str)
 {
 	long int len = 0;
 
-	while (str)
+	while (str[len])
 		len++;
 	return (len);
 }
@@ -34,7 +34,7 @@ char *str_cspn(char *str)
 }
 
 /**
- * str_ncpm - function that compares two strings
+ * str_ncpm - function that compares two strings untill '='
  * @s1: pointer to the 1st string (environment variable)
  * @s2: pointer to the 2nd string (the one fron envi)
  * Return: 0 if there is a match -1 otherwise
@@ -72,4 +72,28 @@ char *str_find(char *str, char dil)
 	if (str[i] == '\0')
 		return (NULL);
 	return (&str[i + 1]);
+}
+
+
+/**
+ * str_cpm - function that compares two strings
+ * @s1: pointer to the 1st string
+ * @s2: pointer to the 2nd string
+ * Return: 0 if there is a match -1 otherwise
+ */
+
+int str_cpm(char *s1, char *s2)
+{
+	int i = 0;
+
+	if (str_len(s2) != str_len(s1))
+		return (-1);
+	else
+	{
+		while ((s1[i] && s2[i]) && (s1[i] == s2[i]))
+			i++;
+	}
+	if (s1[i] == '\0' && s2[i] == '\0')
+		return (0);
+	return (-1);
 }
