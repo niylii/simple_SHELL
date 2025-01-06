a16a161d45319805aba807da5e02ac04d216eb3d
nouhailaaleen <173621209+niylii@users.noreply.github.com>
2025-01-06 16:01:26 +0100

    [beety - under 40 + function added]

--- get_line.c
+++ get_line.c
@@ -17,18 +17,34 @@ void getline_check(ssize_t stat, char *cmd)
 }
 
 /**
- * get_line - a function that mimics the getline function's behaviour
- * @line: a buffer where the line is stored
- * @size: the size of 
- * @stm: the file stream
- * Return: returns number of characters read (success)
- *		-1 (failure)
+ * read_into - Reads data from the file descriptor into the buffer.
+ * @fd: File descriptor to read from.
+ * @buff: Static buffer to store read data.
+ * @bytes: Pointer to the number of bytes read.
+ * @pose: Pointer to the current position in the buffer.
+ * Return: Number of bytes read, or -1 on failure.
  */
 
+static ssize_t read_into(int fd, char *buff, ssize_t *bytes, ssize_t *pose)
+{
+		*bytes = read(fd, buff, 1024);
+		if (*bytes == -1)
+			return (-1);
+		*pose = 0;
+		return (*bytes);
+}
+
+/**
+ * get_line - a function that mimics the getline function
+ * @line: a buffer where the line is stored
+ * @n: the size
+ * @fd: the file stream
+ * Return: returns number of read chars(success), -1 (f)
+ */
 ssize_t get_line(char **line, size_t *n, int fd)
 {
 	static char buff[1024];
-	static ssize_t bytes = 0, pose = 0; 
+	static ssize_t bytes = 0, pose = 0;
 	ssize_t len = 0;
 	char *line_;
 
@@ -45,19 +61,13 @@ ssize_t get_line(char **line, size_t *n, int fd)
 	{
 		if (pose >= bytes)
 		{
-			bytes = read(fd, buff, 1024);
-			if (bytes == -1)
-			{
-				if (len == 0)
-					return (-1);
-				break;
-			}
-			pose = 0;
+			if (read_into(fd, buff, &bytes, &pose) == -1)
+				return ((len == 0) ? -1 : len);
 		}
 		if (len + 1 >= *n)
 		{
 			*n *= 2;
-			line_ = realloc(line_, *n);
+			line_ = realloc(*line, *n);
 			if (!line_)
 				return (-1);
 			*line = line_;
