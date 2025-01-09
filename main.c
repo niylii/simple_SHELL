bace2785926f1be09aac93f5e852da6410e35df9
nouhailaaleen <173621209+niylii@users.noreply.github.com>
2025-01-09 23:22:17 +0100

    entry point of the program

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+ * main - Entry point of the program.
+ *
+ * This function calls the simple_shell10 function to execute the shell.
+ * It returns 0 upon successful completion.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	simple_shell10();
